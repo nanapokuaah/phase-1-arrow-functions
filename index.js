de3fe23e249e5202daa0736f3062feef9f9c66df
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-10-23 07:18:35 -0400

    commit tree 4c915b093d741b427f7efcefb728f480053e2dca

---
+++ index.js
@@ -0,0 +1,19 @@
+function divide(x,y){
+return (x/y);
+}
+
+divide (2000,100)
+
+square = () =>{
+ //has an arrow function called square
+}
+
+let square = (a) => {return a * a;}
+// square arrow function takes one parameter and multiplies it times itself
+
+add = () => {
+//has an arrow function called add
+}
+
+let add = (s,b) => {return s + b;}
+//add arrow function takes two parameters and adds them together
